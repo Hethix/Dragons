dd9c5d32bf39f7375a9e2b9ff8676e03a12d669c
Andreas Ryge <An.ryge@gmail.com>
2017-07-11 13:45:20 +0200

    Movement of character and camera
    Character is only point and click on nav mesh

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+    [SerializeField]
+    private Transform targetPosition;
+    private bool fixedCamera;
+
+	// Use this for initialization
+	void Start () {
+        fixedCamera = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //Camera movement
+        if (fixedCamera) {
+            transform.position = targetPosition.position;
+        } else
+        {
+            if(Input.GetAxis("Horizontal") > 0.1)
+            {
+                transform.position += transform.TransformDirection(Vector3.right) * 10 * Time.fixedDeltaTime;
+            } else if(Input.GetAxis("Horizontal") < -0.1)
+            {
+                transform.position += -transform.TransformDirection(Vector3.right) * 10 * Time.fixedDeltaTime;
+            }
+
+            if (Input.GetAxis("Vertical") > 0.1f)
+            {
+                transform.position += transform.TransformDirection(Vector3.forward) * 10 * Time.fixedDeltaTime;
+            }
+            else if (Input.GetAxis("Vertical") < -0.1f)
+            {
+                transform.position += -transform.TransformDirection(Vector3.forward) * 10 * Time.fixedDeltaTime;
+            }
+        }
+
+        //Change camera mode between fixed to character and free
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            fixedCamera = !fixedCamera;
+        }
+
+        //Camera Rotation
+        if(Input.GetAxis("Rotate") < 0 || Input.GetAxis("Rotate") > 0)
+        {
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Rotate") * 50 * Time.fixedDeltaTime, 0);
+        }
+	}
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Character : MonoBehaviour {
+
+    private NavMeshAgent navMeshAgent;
+
+    // Use this for initialization
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetButtonDown("LeftClick"))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navMeshAgent.destination = hit.point;
+                navMeshAgent.Resume();
+            }
+        }
+    }
+}
